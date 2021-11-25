edd8259abb9e36fb4d93476f002aa1f2713a0636
318511 <91246969+318511@users.noreply.github.com>
2021-11-25 15:41:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,54 @@
+input.onButtonPressed(Button.A, function () {
+    while (!(input.buttonIsPressed(Button.B))) {
+        for (let B = 0; B <= 4; B++) {
+            for (let A = 0; A <= 4; A++) {
+                if (list[B][A] == 1) {
+                    led.plotBrightness(B, A, light2)
+                    light2 += n
+                    if (light2 <= 0) {
+                        n = 5
+                    } else {
+                        if (light2 >= 255) {
+                            n = -5
+                        }
+                    }
+                } else {
+                    led.unplot(B, A)
+                }
+            }
+        }
+        list.unshift(list.pop())
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (!(input.buttonIsPressed(Button.A))) {
+        for (let B = 0; B <= 4; B++) {
+            for (let A = 0; A <= 4; A++) {
+                if (list[A][B] == 1) {
+                    led.plotBrightness(B, A, light2)
+                    light2 += n
+                    if (light2 <= 0) {
+                        n = 5
+                    } else {
+                        if (light2 >= 255) {
+                            n = -5
+                        }
+                    }
+                } else {
+                    led.unplot(B, A)
+                }
+            }
+        }
+        list.unshift(list.pop())
+        basic.pause(100)
+    }
+})
 let n = 0
-let light2 = 255
-let list = [
+let list: number[][] = []
+let light2 = 0
+light2 = 255
+list = [
 [
 0,
 1,
@@ -37,24 +85,3 @@ let list = [
 1
 ]
 ]
-basic.forever(function () {
-    for (let B = 0; B <= 4; B++) {
-        for (let A = 0; A <= 4; A++) {
-            if (list[B][A] == 1) {
-                led.plotBrightness(B, A, light2)
-                light2 += n
-                if (light2 <= 0) {
-                    n = 5
-                } else {
-                    if (light2 >= 255) {
-                        n = -5
-                    }
-                }
-            } else {
-                led.unplot(B, A)
-            }
-        }
-    }
-    list.unshift(list.pop())
-    basic.pause(100)
-})
