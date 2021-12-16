6d187655a6c147777371e02b20dd75a0045bcd6e
318511 <91246969+318511@users.noreply.github.com>
2021-12-16 16:39:53 +0800

    還原「2021/12/2 下午4:07:03 更新 main.blocks, main.ts」

--- main.ts
+++ main.ts
@@ -45,30 +45,40 @@ let list2 = [
 ]
 ]
 basic.forever(function () {
-    for (let A = 0; A <= 4; A++) {
+    if (哈哈 == 1) {
+        let list: number[][] = []
         for (let B = 0; B <= 4; B++) {
-            if (list2[B][A] == 1) {
-                led.plotBrightness(B, A, light2)
-                light2 += n
-                if (light2 <= 0) {
-                    n = 5
-                } else if (light2 >= 255) {
-                    n = -5
+            for (let A = 0; A <= 4; A++) {
+                if (list2[B][A] == 1) {
+                    led.plotBrightness(B, A, light2)
+                    light2 += n
+                    if (light2 <= 0) {
+                        n = 5
+                    } else if (light2 >= 255) {
+                        n = -5
+                    }
+                } else {
+                    led.unplot(B, A)
                 }
-            } else {
-                led.unplot(B, A)
             }
         }
-    }
-    if (哈哈 == 1) {
-        for (let B = 0; B <= 4; B++) {
-            list2.unshift(list2.pop())
-        }
+        list2.unshift(list.pop())
         basic.pause(100)
     } else if (哈哈 == 2) {
         for (let B = 0; B <= 4; B++) {
-            list2.push(light2.shift())
+            for (let A = 0; A <= 4; A++) {
+                if (list2[A][B] == 1) {
+                    led.plotBrightness(B, A, light2)
+                    light2 += n
+                    if (light2 <= 0) {
+                        n = 5
+                    } else if (light2 >= 255) {
+                        n = -5
+                    }
+                } else {
+                    led.unplot(B, A)
+                }
+            }
         }
-        basic.pause(100)
     }
 })
